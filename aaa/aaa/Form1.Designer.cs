3c3ca0625650362a39f61eb5b9c514146a987f75
Ali <74418137+snewstarlet@users.noreply.github.com>
2022-01-06 15:53:43 +0300

    Add files via upload

---
+++ aaa/aaa/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace aaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(831, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 59);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(975, 454);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(149, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Log";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(194, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(300, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "514";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Server Ip";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(297, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Port";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(912, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 44);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 525);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "snewstarlet\'s syslog client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ aaa/aaa/Form1.cs
@@ -0,0 +1,120 @@
+﻿/*
+     Name:		syslog client
+     Created:	20.09.2021
+     Author:	snewstarlet
+ */
+
+
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading;
+using SyslogNet.Client;
+using SyslogNet.Client.Serialization;
+using SyslogNet.Client.Transport;
+
+namespace aaa
+{
+    public partial class Form1 : Form
+    {
+        
+        public string log = "";
+        public int i;
+        public string ip;
+        public int port;
+
+        private SyslogUdpSender _syslogSender;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Form.CheckForIllegalCrossThreadCalls = false;
+            comboBox1.Items.Add("System");
+            comboBox1.Items.Add("Application");
+            comboBox1.Items.Add("Security");
+            comboBox1.Items.Add("Setup");
+            comboBox1.Items.Add("Forwarded Events");
+            listView1.View = View.Details;
+            listView1.Columns.Add("Index", 50, HorizontalAlignment.Left);
+            listView1.Columns.Add("InstanceId", 80, HorizontalAlignment.Left);
+            listView1.Columns.Add("Message", 300 , HorizontalAlignment.Left);
+            listView1.Columns.Add("Source", 280, HorizontalAlignment.Left);
+            listView1.Columns.Add("Time Generated", 120, HorizontalAlignment.Left);
+
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                log = comboBox1.Text;
+                Thread trd = new Thread(new ThreadStart(this.vericek));
+                trd.IsBackground = true;
+                trd.Start();
+                button1.Enabled = false;
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show("Hata: " + err.Message);
+                button1.Enabled = true;
+            }
+        }
+
+        public void vericek()
+        {
+            ip = textBox1.Text;
+            port = Int32.Parse(textBox2.Text);
+            while (true)
+            {
+                try
+                {
+                    EventLog demoLog = new EventLog(log);
+                    EventLogEntryCollection entries = demoLog.Entries;
+                    DateTime dt = DateTime.Now.AddSeconds(-1);
+                    foreach (EventLogEntry entry in entries)
+                    {
+                        if (entry.TimeGenerated > dt && i != entry.Index)
+                        {   
+                            int sira = listView1.Items.Count;
+                            listView1.Items.Add(entry.Index.ToString());
+                            listView1.Items[sira].SubItems.Add(entry.InstanceId.ToString());
+                            listView1.Items[sira].SubItems.Add(entry.Message.ToString());
+                            listView1.Items[sira].SubItems.Add(entry.Source.ToString());
+                            listView1.Items[sira].SubItems.Add(entry.TimeGenerated.ToString());
+                            i = entry.Index;
+                            string msg = entry.EntryType.ToString() +" | "+ entry.Message.ToString() + " | " + entry.Source.ToString();
+                            _syslogSender = new SyslogUdpSender(ip, port);
+                            _syslogSender.Send(
+                                new SyslogMessage(
+                                    entry.TimeGenerated,
+                                    Facility.SecurityOrAuthorizationMessages1,
+                                    Severity.Informational,
+                                    Environment.MachineName,
+                                    "snewstarlet's syslog client ",
+                                    msg),
+                                new SyslogRfc3164MessageSerializer());
+                        }
+                    }
+                }
+                catch (Exception err)
+                {
+
+                    MessageBox.Show("Hata: " + err.Message);
+                    button1.Enabled = true;
+                    break;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+    }
+}
